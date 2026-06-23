f57cfb92f54507cee7a54c2ec130ae9ab2c8458e
V.Nandhakumar <162693004+Mr-tamilan-yt@users.noreply.github.com>
2026-06-23 14:25:52 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Featured from "./components/Featured";
+import Featured from "./components/Featured"; 
 
 export default function App() {
   return (
